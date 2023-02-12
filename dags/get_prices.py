4f3953b7006652ae089f7bac0356ba37a3c7dbcc
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2023-02-12 09:36:06 +0700

    Update get_prices.py

--- dags/get_prices.py
+++ dags/get_prices.py
@@ -1,27 +1,33 @@
+
 import time
 import vnstock
 from datetime import date,timedelta
 from pyspark.sql import SparkSession
 import requests
 import json
-from pyspark.sql.functions import udf, col, explode,lit
+from pyspark.sql.functions import udf, col, explode, lit,substring, to_date, min
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, ArrayType,FloatType,TimestampType
 from pyspark.sql import Row
 from pyspark import SparkContext
 
 tickers = vnstock.listing_companies().ticker
-fd = int(time.mktime(time.strptime((date.today()- timedelta(days=10)).strftime("%Y-%m-%d"), "%Y-%m-%d")))
-td = int(time.mktime(time.strptime((date.today()- timedelta(days=2)).strftime("%Y-%m-%d"), "%Y-%m-%d")))
+fd = int(time.mktime(time.strptime((date.today()- timedelta(days=30)).strftime("%Y-%m-%d"), "%Y-%m-%d")))
+td = int(time.mktime(time.strptime((date.today()- timedelta(days=3)).strftime("%Y-%m-%d"), "%Y-%m-%d")))
 
-body = json.dumps({
-})
+def path_is_readable(spark_session, x):
+  try:
+    spark_session.read.parquet(x)
+    return True
+  except:
+    return False
+
 
 # response function - udf
-def executeRestApi(url, body):
+def executeRestApi(url):
   res = None
   # Make API request, get response object back, create dataframe from above schema.
   try:
-    res = requests.get(url, data=body)
+    res = requests.get(url)
   except Exception as e:
     return e
 
@@ -51,11 +57,11 @@ udf_executeRestApi = udf(executeRestApi, schema)
 spark = SparkSession.builder.appName("UDF REST Demo").getOrCreate()
 
 # requests
-RestApiRequest = Row("url", "body")
+RestApiRequest = Row("url")
 request_df = spark.createDataFrame([
-            RestApiRequest('https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={}&type=stock&resolution=D&from={}&to={}'.format(ticker, fd, td) , body) for ticker in tickers[:2]
+            RestApiRequest('https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={}&type=stock&resolution=D&from={}&to={}'.format(ticker, fd, td) ) for ticker in ["SAM"]
           ])\
-          .withColumn("execute", udf_executeRestApi(col("url"), col("body")))
+          .withColumn("execute", udf_executeRestApi(col("url")))
 request_df_collected = request_df.collect() #write to hdfs
 
 schema = StructType([
@@ -69,12 +75,16 @@ schema = StructType([
 for row in request_df_collected:
   queries =spark.createDataFrame(
        row.execute.data,schema).withColumn("ticker_name", lit(row.execute.ticker)) \
-       .select("close_price","high_price","low_price","open_price","ticker_name","trading_date","volume").collect()
-  try:
-    collect_file_df = spark.read.json("hdfs://node-master:9000/test/{}.json".format(row.execute.ticker)).collect()
-  except:
-    collect_file_df = [row]
-  finally		:
-    final = spark.createDataFrame(collect_file_df + queries)
-    final.drop_duplicates(subset = ['trading_date']) \
-    .write.mode("overwrite").json("hdfs://node-master:9000/test/{}.json".format(row.execute.ticker))
+       .select("close_price","high_price","low_price","open_price","ticker_name","trading_date","volume")
+
+  
+  if path_is_readable(spark, "hdfs://node-master:9000/test/{}.json".format(row.execute.ticker)):
+    file = spark.read.json("hdfs://node-master:9000/test/{}.json".format(row.execute.ticker))
+    file_latest_date = file.withColumn("date",to_date(substring('trading_date',0,10))).select('date').rdd.max()[0]
+    final = queries.withColumn("date",to_date(substring('trading_date',0,10))).filter(col("date")>file_latest_date).drop("date")
+    final.write.mode("append").json("hdfs://node-master:9000/test/{}.json".format(row.execute.ticker))
+  else:
+    queries.write.mode("append").json("hdfs://node-master:9000/test/{}.json".format(row.execute.ticker))
+
+# # spark.stop()
+
